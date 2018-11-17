6f4c6b12711f8a6b219c222410af70f977e826bd
Joanna Henzel <henzel.joanna@gmail.com>
2018-11-17 23:57:31 +0100

    Add taking area with text and recognize UIC

--- get_text_areas.py
+++ get_text_areas.py
@@ -1,15 +1,21 @@
 import cv2
+from ocr_test import get_UIC_from_photo
+import os
 try:
     from PIL import Image
 except ImportError:
     import Image
 
-path = "/home/joanna/Dokumenty/Projekty/Prywatne/skyhacks/0_56_left_26.jpg"
+path = "/home/joanna/Dokumenty/Projekty/Prywatne/skyhacks/0_0_left_56.jpg"
 
 image = cv2.imread(path)
 
 def get_text_areas(image):
-
+    """
+    Get distinctive areas from image.
+    :param image: cv2 image
+    :return: list of images with selected areas
+    """
     # Prepare images
     img2gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     ret, mask = cv2.threshold(img2gray, 180, 255, cv2.THRESH_BINARY)
@@ -24,6 +30,12 @@ def get_text_areas(image):
     _, contours, hierarchy = cv2.findContours(dilated, cv2.RETR_EXTERNAL,
                                               cv2.CHAIN_APPROX_NONE)  # findContours returns 3 variables for getting contours
 
+    # Get dimmensions for new area
+    x1 = None
+    x2 = None
+    y1 = None
+    y2 = None
+
     best_contours = []
     for contour in contours:
         # get rectangle bounding contour
@@ -32,12 +44,46 @@ def get_text_areas(image):
         # Don't choose small or to large false positives that aren't text
         if w < 100 or h < 35 or h > 100:
             continue
-        # # draw rectangle around contour on original image
-        # cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 255), 2)
+        # draw rectangle around contour on original image
+        cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 255), 2)
 
-        cropped_text_area = image[y:y+h, x:x+w]
+        margin = 20
+        y1 = (y-margin) if (y-margin) >= 0 else 0
+        y2 = (y+h+margin) if (y+h+margin) <= image.shape[0] else image.shape[0]
+        x1 = (x-margin) if (x-margin) >= 0 else 0
+        x2 = (x+w+margin) if (x+w+margin) <= image.shape[1] else image.shape[1]
+        cropped_text_area = image[y1:y2, x1:x2]
         best_contours.append(cropped_text_area)
+
+    # cv2.imshow("areas", image)
+    # cv2.waitKey()
     # write original image with added contours
     return(best_contours)
 
-cropped_images = get_text_areas(image)
+
+def get_uic_from_full_image(image):
+    cropped_images = get_text_areas(image)
+    detected_uic = []
+    for img in cropped_images:
+        detected_uic.append(get_UIC_from_photo(img))
+    pass
+    detected_uic = list(filter(None, detected_uic))
+    # length = len(detected_uic)
+    # if (length < 1):
+    #     return(None)
+    return(detected_uic)
+
+file_path="./data/Training/Training/0_2/0_2_right/"
+for filename in sorted(os.listdir(file_path)):
+    if filename.endswith(".jpg") :
+        image = cv2.imread(file_path+filename)
+        UIC= get_uic_from_full_image(image)
+        # if len(UIC) > 1:
+        #     print(filename + ": " + UIC)
+        # else:
+        #     print(filename + "failed")
+        # continue
+        print(filename)
+        print(UIC)
+    else:
+        continue
--- ocr_test.py
+++ ocr_test.py
@@ -1,37 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
-from PIL import Image
 import pytesseract
-import os
 import cv2 as cv
 import numpy as np
 import re
-from IPython.display import display
-
-filename = "0_42_left_57.jpg"
-# Simple image to string
 
 pattern = re.compile("(\d{7}\-\d{1})")
 
-def get_UIC_from_photo(folder, filename):
-#    ocr2 = pytesseract.image_to_string(Image.open(filename))
-    img = cv.imread(folder + filename,0)
+def get_UIC_from_photo(img):
+
     img = cv.medianBlur(img,3)
     prog = np.average(img)*1.3
     ret,img = cv.threshold(img,prog,255,cv.THRESH_BINARY_INV)
-#    img = cv2.resize(img, None, fx=5, fy=5)
-#    ocr1 = pytesseract.image_to_string(filename)    # Convert to gray
-#    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-#    cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
-#    kernel = np.ones((4, 4), np.uint8)
-#    img = cv2.erode(img, kernel, iterations=2)
-    ocr1 = pytesseract.image_to_string(img)#, config='--psm 3')
-#    cv2.imwrite('test1.jpg', img)
-#    print(ocr1)
-#    if ocr1 != ocr2:
-#        print(ocr1)
-#        print(ocr2)
+    ocr1 = pytesseract.image_to_string(img)
+
     ocr1 = re.sub('~', '-',ocr1)
     ocr1 = re.sub('[A-Za-z (\n)]', '',ocr1)
     
@@ -43,22 +25,3 @@ def get_UIC_from_photo(folder, filename):
     if len(ocr1)>7:
         return '999'
     return ""
-        
-
-#print(get_UIC_from_photo('test1.jpg'))
-UIC_list = list()
-folder = os.getcwd() + '/'
-folder = "/media/dawid/Dane 2_2/Projekty/Python/Hackathon/Training/Training/0_4/0_4_left/"
-for filename in os.listdir(folder):
-    if filename.endswith(".jpg") :
-        UIC= get_UIC_from_photo(folder, filename)
-        if len(UIC) > 1:
-            print(filename + ": " + UIC)
-        else:            
-            print(filename + " failed")
-            
-        if len(UIC) > 8:
-            UIC_list.append(filename + ": " + UIC)
-        continue
-    else:
-        continue
